52be9ef5a5ceac305339443f140f0455248bb19e
benyaminsobirin <benyaminsobirin17@gmail.com>
2022-12-16 20:35:28 +0700

    first commit

--- src/elements/Button/index.test.js
+++ src/elements/Button/index.test.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { BrowserRouter as Router} from 'react-router-dom'
 import Button from './index'
 
 test("Jangan Mengklik Tombol jika kondisi Mati", () => {
     const {container} = render(<Button isDisabled></Button>)
 
     expect(container.querySelector('span.disabled')).toBeInTheDocument()
+})
+
+test("should render loading", () => {
+    const { container,getByText } = render(<Button isLoading></Button>);
+
+    expect(getByText(/Loading/i))
+
+    expect(container.querySelector('span')).toBeInTheDocument()
+})
+
+test("should render <a> tag", () => {
+    const { container } = render(<Button type="link" isExternal></Button>);
+
+    expect(container.querySelector('a')).toBeInTheDocument()
+})
+
+test("should render <Link> component", () => {
+    const { container } = render(<Router>
+        <Button href="" type="link" isExternal></Button>
+        </Router>);
+
+    expect(container.querySelector('a')).toBeInTheDocument()
 })
